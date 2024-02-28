1cf32919adbed8121374f2ed196f25894d69b2de
rfrei <p155247@infong32.clienthosting.eu>
2024-02-28 14:40:59 +0100

    test

---
+++ functions.php
@@ -0,0 +1,265 @@
+<?php
+
+/**
+ * Radio F.R.E.I. functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Radio F.R.E.I.
+ * @since Radio F.R.E.I. 1.0
+ */
+
+/**
+ * Register block styles.
+ */
+add_action('init', 'radiofrei_block_styles');
+function radiofrei_block_styles()
+{
+
+	register_block_style(
+		'core/details',
+		array(
+			'name'         => 'arrow-icon-details',
+			'label'        => __('Arrow icon', 'radiofrei'),
+			/*
+				 * Styles for the custom Arrow icon style of the Details block
+				 */
+			'inline_style' => '
+				.is-style-arrow-icon-details {
+					padding-top: var(--wp--preset--spacing--10);
+					padding-bottom: var(--wp--preset--spacing--10);
+				}
+
+				.is-style-arrow-icon-details summary {
+					list-style-type: "\2193\00a0\00a0\00a0";
+				}
+
+				.is-style-arrow-icon-details[open]>summary {
+					list-style-type: "\2192\00a0\00a0\00a0";
+				}',
+		)
+	);
+	register_block_style(
+		'core/post-terms',
+		array(
+			'name'         => 'pill',
+			'label'        => __('Pill', 'radiofrei'),
+			/*
+				 * Styles variation for post terms
+				 * https://github.com/WordPress/gutenberg/issues/24956
+				 */
+			'inline_style' => '
+				.is-style-pill a,
+				.is-style-pill span:not([class], [data-rich-text-placeholder]) {
+					display: inline-block;
+					background-color: var(--wp--preset--color--base-2);
+					padding: 0.375rem 0.875rem;
+					border-radius: var(--wp--preset--spacing--20);
+				}
+
+				.is-style-pill a:hover {
+					background-color: var(--wp--preset--color--contrast-3);
+				}',
+		)
+	);
+	register_block_style(
+		'core/list',
+		array(
+			'name'         => 'checkmark-list',
+			'label'        => __('Checkmark', 'radiofrei'),
+			/*
+				 * Styles for the custom checkmark list block style
+				 * https://github.com/WordPress/gutenberg/issues/51480
+				 */
+			'inline_style' => '
+				ul.is-style-checkmark-list {
+					list-style-type: "\2713";
+				}
+
+				ul.is-style-checkmark-list li {
+					padding-inline-start: 1ch;
+				}',
+		)
+	);
+	register_block_style(
+		'core/navigation-link',
+		array(
+			'name'         => 'arrow-link',
+			'label'        => __('With arrow', 'radiofrei'),
+			/*
+				 * Styles for the custom arrow nav link block style
+				 */
+			'inline_style' => '
+				.is-style-arrow-link .wp-block-navigation-item__label:after {
+					content: "\2197";
+					padding-inline-start: 0.25rem;
+					vertical-align: middle;
+					text-decoration: none;
+					display: inline-block;
+				}',
+		)
+	);
+	register_block_style(
+		'core/heading',
+		array(
+			'name'         => 'asterisk',
+			'label'        => __('With asterisk', 'radiofrei'),
+			'inline_style' => "
+				.is-style-asterisk:before {
+					content: '';
+					width: 1.5rem;
+					height: 3rem;
+					background: var(--wp--preset--color--contrast-2, currentColor);
+					clip-path: path('M11.93.684v8.039l5.633-5.633 1.216 1.23-5.66 5.66h8.04v1.737H13.2l5.701 5.701-1.23 1.23-5.742-5.742V21h-1.737v-8.094l-5.77 5.77-1.23-1.217 5.743-5.742H.842V9.98h8.162l-5.701-5.7 1.23-1.231 5.66 5.66V.684h1.737Z');
+					display: block;
+				}
+
+				/* Hide the asterisk if the heading has no content, to avoid using empty headings to display the asterisk only, which is an A11Y issue */
+				.is-style-asterisk:empty:before {
+					content: none;
+				}
+
+				.is-style-asterisk:-moz-only-whitespace:before {
+					content: none;
+				}
+
+				.is-style-asterisk.has-text-align-center:before {
+					margin: 0 auto;
+				}
+
+				.is-style-asterisk.has-text-align-right:before {
+					margin-left: auto;
+				}
+
+				.rtl .is-style-asterisk.has-text-align-left:before {
+					margin-right: auto;
+				}",
+		)
+	);
+}
+
+
+/**
+ * Enqueue custom block stylesheets
+ *
+ * @since Radio F.R.E.I. 1.0
+ * @return void
+ */
+add_action('init', 'radiofrei_block_stylesheets');
+function radiofrei_block_stylesheets()
+{
+	/**
+	 * The wp_enqueue_block_style() function allows us to enqueue a stylesheet
+	 * for a specific block. These will only get loaded when the block is rendered
+	 * (both in the editor and on the front end), improving performance
+	 * and reducing the amount of data requested by visitors.
+	 *
+	 * See https://make.wordpress.org/core/2021/12/15/using-multiple-stylesheets-per-block/ for more info.
+	 */
+	wp_enqueue_block_style(
+		'core/button',
+		array(
+			'handle' => 'radiofrei-button-style-outline',
+			'src'    => get_parent_theme_file_uri('assets/css/button-outline.css'),
+			'ver'    => wp_get_theme(get_template())->get('Version'),
+			'path'   => get_parent_theme_file_path('assets/css/button-outline.css'),
+		)
+	);
+}
+
+
+/**
+ * Register pattern categories
+ *
+ * @since Radio F.R.E.I. 1.0
+ * @return void
+ */
+add_action('init', 'radiofrei_pattern_categories');
+function radiofrei_pattern_categories()
+{
+
+	register_block_pattern_category(
+		'page',
+		array(
+			'label'       => _x('Pages', 'Block pattern category'),
+			'description' => __('A collection of full page layouts.'),
+		)
+	);
+}
+
+
+/**
+ * unterstützung style.css
+ *
+ * @return void
+ */
+add_action('after_setup_theme', 'radiofrei_support');
+function radiofrei_support()
+{
+	// Enqueue editor styles.
+	add_editor_style('style.css');
+}
+
+/**
+ * unterstützung style.css
+ *
+ * @return void
+ */
+add_action('wp_enqueue_scripts', 'radiofrei_styles');
+function radiofrei_styles()
+{
+
+	// Register theme stylesheet.
+	wp_register_style(
+		'radiofrei-style',
+		get_template_directory_uri() . '/style.css',
+		array(),
+		wp_get_theme()->get('Version')
+	);
+
+	// Enqueue theme stylesheet.
+	wp_enqueue_style('radiofrei-style');
+}
+
+
+
+
+/* Disable WordPress Admin Bar on frontend for all users */
+//add_filter('show_admin_bar', '__return_false');
+
+
+/**
+ * Allows shortcodes to evaluate special magic tags which appear like magic tags,
+ * but the values are mapped through pods_v(). Use this with caution.
+ */
+define('PODS_SHORTCODE_ALLOW_EVALUATE_TAGS', true);
+
+
+/**
+ * Hilfsfunktionen
+ */
+
+function rf_log($data)
+{
+	if (true === WP_DEBUG) {
+		if (is_array($data) || is_object($data)) {
+			error_log(print_r($data, true));
+		} else {
+			if (!empty($data)) {
+				error_log($data);
+			}
+		}
+	}
+}
+
+/*
+ * Includes
+ */
+
+// include funktionen startseite
+include('includes/startseite.php');
+
+// include funktionen programmschema
+include('includes/wochenprogramm.php');
+
+
---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== Radio F.R.E.I. ===
+Contributors: Freies Radio Erfurt e.V.
+Requires at least: 6.0
+Tested up to: 6.4.3
+Requires PHP: 5.7
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release
+
+== Copyright ==
+
+Radio F.R.E.I. basiert auf Twenty Twenty-Four (https://wordpress.org/themes/twentytwentyfour/), (C) the WordPress team, [GPLv2 or later](http://www.gnu.org/licenses/gpl-2.0.html)
+
+Twenty Twenty-Four WordPress Theme, (C) 2023 WordPress.org
+Twenty Twenty-Four is distributed under the terms of the GNU GPL.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+=== Images ===
+License: CC0 https://creativecommons.org/publicdomain/zero/1.0/
+...
+
+License: UNICODE LICENSE V3 https://www.unicode.org/license.txt
+icon-message.webp
+
