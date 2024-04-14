f1902dfe8be5f04c53dae64e3b1103ee7cd3e285
rfrei <p155247@infong32.clienthosting.eu>
2024-04-14 13:15:55 +0200

    wochenprogramm circle play buttons

--- pods-views/rf-wochenprogramm.php
+++ pods-views/rf-wochenprogramm.php
@@ -63,6 +63,8 @@ if (!empty($rows)) {
                         <table class="has-border-color has-contrast-2-border-color" style="border-width:1px">
                             <?php
                             foreach ($rows_for_one_weekday as $key => $row) {
+                                $url = site_url() . '/sendung/' . $row->permalink;
+                                $img = wp_get_attachment_image_url($row->bild);
                                 if ($key == 0) {
                                     // erste zeile: kopfzeile
                             ?>
@@ -79,26 +81,21 @@ if (!empty($rows)) {
                                         <td colspan="3"><?php echo rf_strip_date($row->start) . ' Uhr'; ?></td>
                                     </tr>
                                     <tr>
-                                        <td style="border-right:none;width:58px;line-height:0;">
-                                            <?php
-                                            // daten ermitteln
-                                            // es wird von der Startzeit nur die Stunde gecheckt (H00)
-                                            $file_src = ABSPATH . 'programm/' . mysql2date('Y/m/d/Y m d H00', $row->start) . ' radiofrei.mp3';
-                                            $src = site_url('/programm/') . mysql2date('Y/m/d/Y m d H00', $row->start) . ' radiofrei.mp3';
-                                            $title = $row->name . ' - ' . rf_strip_time($row->start);
-                                            $url = site_url() . '/sendung/' . $row->permalink;
-                                            $img = wp_get_attachment_image_url($row->bild);
-                                            ?>
+                                        <td style="border-right:none;width:58px;line-height:0;vertical-align:top;">
                                             <a href="<?php echo $url; ?>"><img width="58" height="58" src="<?php echo $img; ?>" /></a>
                                         </td>
-                                        <td style="border-left:none;padding-left:2px;vertical-align:top;">
+                                        <td style="border-left:none;vertical-align:top;padding-left:0px;">
                                             <a href="<?php echo $url; ?>"><?php echo $row->name; ?></a>
                                             <?php
+                                            // es wird von der Startzeit nur die Stunde gecheckt (H00)
+                                            $file_src = ABSPATH . 'programm/' . mysql2date('Y/m/d/Y m d H00', $row->start) . ' radiofrei.mp3';
                                             // gibt es einen mitschnitt?
                                             if (file_exists($file_src)) {
+                                                $src = site_url('/programm/') . mysql2date('Y/m/d/Y m d H00', $row->start) . ' radiofrei.mp3';
+                                                $title = $row->name . ' - ' . rf_strip_time($row->start);
                                             ?>
-                                                <div style="display:flex;justify-content:right;margin-top:5px;">
-                                                    <?php echo rf_create_audio_button($src, $title, $url, $img, 'Hören'); ?>
+                                                <div style="line-height:0">
+                                                    <img onclick="rf_playItem(event)" data-src="<?php echo $src; ?>" data-title="<?php echo $title; ?>" data-url="<?php echo $url; ?>" data-img="<?php echo $img; ?>" class="rf-circle-play-button" width="24" height="24" src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cpath fill='white' d='M8 5.14v14l11-7z'/%3E%3C/svg%3E" />
                                                 </div>
                                             <?php
                                             } // ende if
